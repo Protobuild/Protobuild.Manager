3b35e2d2538a3b1631b2d0f6b56058784de49a8a
June Rhodes <jrhodes@redpointgames.com.au>
2015-12-10 21:44:23 +1100

    Install templates into the Visual Studio Experimental instance as well

--- Protobuild.Manager/IDEProjectTemplateSync/VisualStudioProjectTemplateSync.cs
+++ Protobuild.Manager/IDEProjectTemplateSync/VisualStudioProjectTemplateSync.cs
@@ -32,58 +32,80 @@ namespace Protobuild.Manager
             {
                 var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
                 path = Path.Combine(path, "Visual Studio " + version, "Templates", "ProjectTemplates", _brandingEngine.TemplateIDECategory, _brandingEngine.ProductStorageID.TrimStart('.'));
+                
+                await InstallTemplates(path);
+            }
 
-                Directory.CreateDirectory(path);
+            var expVsVersions = new[] {"14.0"};
 
-                foreach (var file in new DirectoryInfo(path).GetFiles("*.vstemplate").ToList())
+            foreach (var version in expVsVersions)
+            {
+                var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                path = Path.Combine(path, "Microsoft", "VisualStudio", version + "Exp", "ProjectTemplates");
+                if (Directory.Exists(path))
                 {
-                    file.Delete();
+                    path = Path.Combine(path, _brandingEngine.TemplateIDECategory,
+                        _brandingEngine.ProductStorageID.TrimStart('.'));
+                    await InstallTemplates(path);
                 }
+            }
+        }
 
-                var assemblyPath = Assembly.GetEntryAssembly().Location;
-                var workingDirectory = Environment.CurrentDirectory;
+        private async Task InstallTemplates(string path)
+        {
+            Directory.CreateDirectory(path);
 
-                // Extract branding icon for Windows.
-                using (
-                    var writer = new FileStream(Path.Combine(path, "Template.ico"), FileMode.Create, FileAccess.Write))
+            foreach (var file in new DirectoryInfo(path).GetFiles("*.vstemplate").ToList())
+            {
+                file.Delete();
+            }
+
+            var assemblyPath = Assembly.GetEntryAssembly().Location;
+            var workingDirectory = Environment.CurrentDirectory;
+
+            // Extract branding icon for Windows.
+            using (
+                var writer = new FileStream(Path.Combine(path, "Template.ico"), FileMode.Create, FileAccess.Write))
+            {
+                _brandingEngine.WindowsIcon.Save(writer);
+            }
+
+            foreach (var template in _templateSource.GetTemplates())
+            {
+                var xml =
+                    "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                    "<VSTemplate Version=\"3.0.0\" Type=\"Project\" xmlns=\"http://schemas.microsoft.com/developer/vstemplate/2005\" xmlns:sdk=\"http://schemas.microsoft.com/developer/vstemplate-sdkextension/2010\">" +
+                    "  <TemplateData>" +
+                    "    <Name>" + SecurityElement.Escape(template.TemplateName) + "</Name>" +
+                    "    <Description>" + SecurityElement.Escape(template.TemplateDescription) + "</Description>" +
+                    "    <Icon>Template.ico</Icon>" +
+                    "    <ProjectType>CSharp</ProjectType>" +
+                    "    <TemplateID>" + SecurityElement.Escape(template.TemplateName) + "</TemplateID>" +
+                    "    <SortOrder>0</SortOrder>" +
+                    "    <DefaultName>" + SecurityElement.Escape(template.TemplateName) + "</DefaultName>" +
+                    "    <ProvideDefaultName>true</ProvideDefaultName>" +
+                    "    <CreateNewFolder>false</CreateNewFolder>" +
+                    "    <LocationField>Disabled</LocationField>" +
+                    "    <CreateInPlace>true</CreateInPlace>" +
+                    "  </TemplateData>" +
+                    "  <TemplateContent>" +
+                    "    <CustomParameters>" +
+                    "      <CustomParameter Name=\"ProtobuildManagerExecutablePath\" Value=\"" +
+                    SecurityElement.Escape(assemblyPath) + "\" />" +
+                    "      <CustomParameter Name=\"ProtobuildManagerWorkingDirectory\" Value=\"" +
+                    SecurityElement.Escape(workingDirectory) + "\" />" +
+                    "      <CustomParameter Name=\"ProtobuildManagerTemplateURL\" Value=\"" +
+                    SecurityElement.Escape(template.TemplateURI) + "\" />" +
+                    "    </CustomParameters>" +
+                    "  </TemplateContent>" +
+                    "  <WizardExtension>" +
+                    "    <Assembly>Protobuild.IDE.VisualStudio, Version=1.0.0.0, Culture=neutral, PublicKeyToken=418e79cef58c6857</Assembly>" +
+                    "    <FullClassName>Protobuild.IDE.VisualStudio.CrossPlatformProjectWizard</FullClassName>" +
+                    "  </WizardExtension>" +
+                    "</VSTemplate>";
+                using (var writer = new StreamWriter(Path.Combine(path, template.TemplateName + ".vstemplate")))
                 {
-                    _brandingEngine.WindowsIcon.Save(writer);
-                }
-
-                foreach (var template in _templateSource.GetTemplates())
-                {
-                    var xml =
-                        "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-                        "<VSTemplate Version=\"3.0.0\" Type=\"Project\" xmlns=\"http://schemas.microsoft.com/developer/vstemplate/2005\" xmlns:sdk=\"http://schemas.microsoft.com/developer/vstemplate-sdkextension/2010\">" +
-                        "  <TemplateData>" +
-                        "    <Name>" + SecurityElement.Escape(template.TemplateName) + "</Name>" +
-                        "    <Description>" + SecurityElement.Escape(template.TemplateDescription) + "</Description>" +
-                        "    <Icon>Template.ico</Icon>" +
-                        "    <ProjectType>CSharp</ProjectType>" +
-                        "    <TemplateID>" + SecurityElement.Escape(template.TemplateName) + "</TemplateID>" +
-                        "    <SortOrder>0</SortOrder>" +
-                        "    <DefaultName>" + SecurityElement.Escape(template.TemplateName) + "</DefaultName>" +
-                        "    <ProvideDefaultName>true</ProvideDefaultName>" +
-                        "    <CreateNewFolder>false</CreateNewFolder>" +
-                        "    <LocationField>Disabled</LocationField>" +
-                        "    <CreateInPlace>true</CreateInPlace>" +
-                        "  </TemplateData>" +
-                        "  <TemplateContent>" +
-                        "    <CustomParameters>" +
-                        "      <CustomParameter Name=\"ProtobuildManagerExecutablePath\" Value=\"" + SecurityElement.Escape(assemblyPath) + "\" />" +
-                        "      <CustomParameter Name=\"ProtobuildManagerWorkingDirectory\" Value=\"" + SecurityElement.Escape(workingDirectory) + "\" />" +
-                        "      <CustomParameter Name=\"ProtobuildManagerTemplateURL\" Value=\"" + SecurityElement.Escape(template.TemplateURI) + "\" />" +
-                        "    </CustomParameters>" +
-                        "  </TemplateContent>" +
-                        "  <WizardExtension>" +
-                        "    <Assembly>Protobuild.IDE.VisualStudio, Version=1.0.0.0, Culture=neutral, PublicKeyToken=418e79cef58c6857</Assembly>" +
-                        "    <FullClassName>Protobuild.IDE.VisualStudio.CrossPlatformProjectWizard</FullClassName>" +
-                        "  </WizardExtension>" +
-                        "</VSTemplate>";
-                    using (var writer = new StreamWriter(Path.Combine(path, template.TemplateName + ".vstemplate")))
-                    {
-                        await writer.WriteAsync(xml);
-                    }
+                    await writer.WriteAsync(xml);
                 }
             }
         }

865b528e7a19b4f0e1b571be72ccd1265e357f50
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-12-13 22:16:42 +1100

    Add other known templates to the built-in list

--- Protobuild.Manager/TemplateSourcing/BuiltinTemplateSource.cs
+++ Protobuild.Manager/TemplateSourcing/BuiltinTemplateSource.cs
@@ -10,6 +10,42 @@ namespace Protobuild.Manager
             {
                 new TemplateInfo
                 {
+                    TemplateName = "C# Console",
+                    TemplateDescription = "A cross-platform console application that can work on Windows, Mac OS and Linux.",
+                    TemplateURI = "http://protobuild.org/commons/Console",
+                    AdditionalProtobuildVariants = new Dictionary<string, VariantOverlay>(),
+                    AdditionalStandardProjectVariants = new Dictionary<string, VariantOverlay>(),
+                    OptionVariants = new List<OptionalVariant>(),
+                },
+                new TemplateInfo
+                {
+                    TemplateName = "C# Library",
+                    TemplateDescription = "A cross-platform C# library that can work across desktop and mobile platforms.",
+                    TemplateURI = "http://protobuild.org/commons/Console",
+                    AdditionalProtobuildVariants = new Dictionary<string, VariantOverlay>(),
+                    AdditionalStandardProjectVariants = new Dictionary<string, VariantOverlay>(),
+                    OptionVariants = new List<OptionalVariant>(),
+                },
+                new TemplateInfo
+                {
+                    TemplateName = "Protogame Empty Game",
+                    TemplateDescription = "A cross-platform Protogame project that renders an blank screen.",
+                    TemplateURI = "http://protobuild.org/Protogame/Template.Blank",
+                    AdditionalProtobuildVariants = new Dictionary<string, VariantOverlay>(),
+                    AdditionalStandardProjectVariants = new Dictionary<string, VariantOverlay>(),
+                    OptionVariants = new List<OptionalVariant>(),
+                },
+                new TemplateInfo
+                {
+                    TemplateName = "Protogame 2D Platformer",
+                    TemplateDescription = "A cross-platform Protogame project that runs a 2D platformer.",
+                    TemplateURI = "http://protobuild.org/Protogame/Template.Platformer",
+                    AdditionalProtobuildVariants = new Dictionary<string, VariantOverlay>(),
+                    AdditionalStandardProjectVariants = new Dictionary<string, VariantOverlay>(),
+                    OptionVariants = new List<OptionalVariant>(),
+                },
+                new TemplateInfo
+                {
                     TemplateName = "MonoGame 2D Platformer",
                     TemplateDescription = "A cross-platform MonoGame project that runs a 2D platformer.",
                     TemplateURI = "http://protobuild.org/MonoGame/Template.2DPlatformer@develop",

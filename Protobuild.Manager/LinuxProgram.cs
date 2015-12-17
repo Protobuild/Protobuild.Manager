1680f2494f68c86a0412685ff17b764ac8f5ac75
June Rhodes <jrhodes@redpointsoftware.com.au>
2015-12-17 17:24:18 +1100

    Handle missing libraries better on Linux

--- Protobuild.Manager/LinuxProgram.cs
+++ Protobuild.Manager/LinuxProgram.cs
@@ -6,6 +6,7 @@ using GLib;
 using Gtk;
 using WebKit;
 using System.IO;
+using System.Linq;
 
 namespace Protobuild.Manager
 {
@@ -40,6 +41,58 @@ namespace Protobuild.Manager
                 {
                     Run(args);
                 }
+                catch (TypeInitializationException e)
+                {
+                    if (e.InnerException != null && e.InnerException.Message.Contains("libwebkitgtk-1.0.so.0"))
+                    {
+                        ShowRequiredLibrary(new[] {
+                            "zypper in libwebkitgtk-1__0-0"
+                        });
+                    }
+                    else if (e.InnerException != null && e.InnerException.Message.Contains("webkit-1.0"))
+                    {
+                        // Try and extract webkit-sharp next to the assembly.
+                        try
+                        {
+                            var assembly = typeof(Program).Assembly.GetManifestResourceStream("webkit-sharp.dll");
+                            var assemblyConfig = typeof(Program).Assembly.GetManifestResourceStream("webkit-sharp.dll.config");
+
+                            using (var writer = new FileStream(Path.Combine(new FileInfo(typeof(Program).Assembly.Location).DirectoryName, "webkit-sharp.dll"), FileMode.Create, FileAccess.Write))
+                            {
+                                assembly.CopyTo(writer);
+                            }
+                            using (var writer = new FileStream(Path.Combine(new FileInfo(typeof(Program).Assembly.Location).DirectoryName, "webkit-sharp.dll.config"), FileMode.Create, FileAccess.Write))
+                            {
+                                assemblyConfig.CopyTo(writer);
+                            }
+
+                            // We have to relaunch the executable to pick up the new libraries next to us.
+                            var process = System.Diagnostics.Process.Start("mono", "\"" + typeof(Program).Assembly.Location + "\" " + string.Join(" ", args));
+                            process.WaitForExit();
+
+                            if (process.ExitCode == 1)
+                            {
+                                File.Delete(Path.Combine(new FileInfo(typeof(Program).Assembly.Location).DirectoryName, "webkit-sharp.dll"));
+                                File.Delete(Path.Combine(new FileInfo(typeof(Program).Assembly.Location).DirectoryName, "webkit-sharp.dll.config"));
+                                ShowRequiredLibrary(new[] {
+                                    "zypper in webkit-sharp"
+                                });
+                            }
+
+                            Environment.Exit(process.ExitCode);
+                        }
+                        catch
+                        {
+                            ShowRequiredLibrary(new[] {
+                                "zypper in webkit-sharp"
+                            });
+                        }
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine(e);
+                    }
+                }
                 catch (System.IO.FileNotFoundException e)
                 {
                     if (e.Message.Contains("webkit-sharp"))
@@ -67,14 +120,18 @@ namespace Protobuild.Manager
                             {
                                 File.Delete(Path.Combine(new FileInfo(typeof(Program).Assembly.Location).DirectoryName, "webkit-sharp.dll"));
                                 File.Delete(Path.Combine(new FileInfo(typeof(Program).Assembly.Location).DirectoryName, "webkit-sharp.dll.config"));
-                                ShowRequiredLibrary("webkit-sharp");
+                                ShowRequiredLibrary(new[] {
+                                    "zypper in webkit-sharp"
+                                });
                             }
 
                             Environment.Exit(process.ExitCode);
                         }
                         catch
                         {
-                            ShowRequiredLibrary("webkit-sharp");
+                            ShowRequiredLibrary(new[] {
+                                "zypper in webkit-sharp"
+                            });
                         }
                     }
                     else
@@ -89,21 +146,28 @@ namespace Protobuild.Manager
             }
         }
 
-        public static void ShowRequiredLibrary(string library)
+        public static void ShowRequiredLibrary(string[] librarySuggestions)
         {
             Application.Init();
             var window = new Window("Missing Library!");
-            window.Icon = new Gdk.Pixbuf(System.Reflection.Assembly.GetExecutingAssembly(), "Unearth.GameIcon.ico");
-            window.SetSizeRequest(300, 150);
+            //window.SetSizeRequest(300, 150);
             window.Destroyed += delegate (object sender, EventArgs e)
             {
                 Application.Quit();
             };
-            var label = new Label("Please install " + library + " using \nyour package manager!");
+            var label = new Label("You are missing a required library to run this application.  Please " +
+                "install the required library using the command appropriate " +
+                "for your Linux distribution:\n" +
+                "\n" +
+                librarySuggestions.Select(a => "* " + a + "\n").Aggregate((a, b) => a + b) +
+                "\n" +
+                "Once you have installed this library, try running the application again.");
+            label.SetPadding(20, 20);
+
             window.Add(label);
             window.WindowPosition = WindowPosition.Center;
-            window.AllowGrow = false;
-            window.AllowShrink = false;
+            //window.AllowGrow = false;
+            //window.AllowShrink = false;
             window.ShowAll();
             Application.Run();
         }

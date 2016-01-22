55bfb19c4166fd53ed0ee8fc7a31c09994335a21
June Rhodes <jrhodes@redpointgames.com.au>
2016-01-22 19:36:37 +1100

    Fix up Mac OS installer
    Fixes #3

--- Installers/MacOS/install.sh
+++ Installers/MacOS/install.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-WGET=$(which wget)
-if [ "$WGET" == "" ]; then
-  echo "wget not found.  You might need to install it with brew install wget."
+CURL=$(which curl)
+if [ "$CURL" == "" ]; then
+  echo "curl not found.  You might need to install it with brew install wget."
   exit 1
 fi
 
 cd ~/
-$WGET -O Protobuild.exe https://protobuild.org/get
+$CURL -L https://protobuild.org/get > Protobuild.exe
 if [ $? -ne 0 ]; then
   # Fallback to HTTP
-  $WGET -O Protobuild.exe http://protobuild.org/get
+  $CURL -L http://protobuild.org/get > Protobuild.exe
 fi
 if [ $? -ne 0 ]; then
   echo "Unable to download Protobuild from the internet"
   exit 1
 fi
 /usr/local/bin/mono Protobuild.exe --install https://protobuild.org/hach-que/Protobuild.Manager
+chmod a+x ~/Applications/Protobuild.Manager.app/Contents/MacOS/Protobuild.Manager
 
 echo "Installation complete."
+
+open -a ~/Applications/Protobuild.Manager.app
